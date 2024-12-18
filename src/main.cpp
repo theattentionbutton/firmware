e40d8a6514c0e85915b49f55030cf3d2be529d01
Siddharth Singh <me@shantaram.xyz>
2024-12-19 04:57:55 +0530

    add display and enc input stuff

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,27 @@ AttentionButton *btn;
 // Create an EventEncoderButton input
 EventEncoderButton enc(ENC_DAT, ENC_CLK,
                        ENC_SW); // First two should be interrupt pins
-// Create a callback handler function
+
+int icon_id = 0;
+unsigned long pos_change = 0;
+bool disp_was_reset = true;
 void on_enc_input(InputEventType ev, EventEncoderButton &b) {
     switch (ev) {
         case InputEventType::CLICKED:
             btn->schedule_request();
             break;
-        case InputEventType::CHANGED:
-            Serial.println(b.position());
+        case InputEventType::CHANGED: {
+            pos_change = millis();
+            disp_was_reset = false;
+            int pos = b.position() % DRAWABLE_LENGTH;
+            int counter = 0;
+            for (int i = 0; i < DRAWABLE_LENGTH; i++) {
+                if (pos == counter) break;
+                counter += 1;
+            }
+            btn->draw_icon_by_id(DRAWABLE_ICONS[counter]);
+            break;
+        }
         default:
             break;
     }
@@ -52,6 +65,13 @@ void loop() {
         }
     } else {
         if (!btn->begun) btn->begin_client_mode();
+    }
+
+    if (now - pos_change > 3000 && !disp_was_reset) {
+        Serial.printf("[loop] Time since last event: %lu - resetting display\n",
+                      now - pos_change);
+        btn->draw_icon("READY");
+        disp_was_reset = true;
     }
 
     if (btn->begun) {
