3a7633d7213946d81d7d74b3111b34b64fef94c6
Siddharth Singh <me@shantaram.xyz>
2024-12-18 16:05:22 +0530

    increase dns interval and add logging

--- src/captive_portal.h
+++ src/captive_portal.h
@@ -16,27 +16,21 @@
       // https://en.wikipedia.org/wiki/List_of_WLAN_channels#2.4_GHz_(802.11b/g/n/ax)
 
 // Define the DNS interval in milliseconds between processing DNS requests
-#define DNS_INTERVAL 30
+#define DNS_INTERVAL 50
 
 void setup_dns_server(DNSServer &dns, const IPAddress &local_ip) {
     // Set the TTL for DNS response and start the DNS server
     dns.setTTL(3600);
-    dns.start(53, "*", local_ip);
+    bool started = dns.start(53, "*", local_ip);
+    Serial.printf("[setup mode] DNS server startup %s\n",
+                  started ? "successful" : "failed");
 }
 
 void start_soft_ap(const char *ssid, const char *password,
                    const IPAddress &local_ip, const IPAddress &gateway_ip) {
-    // Set the WiFi mode to access point and station
     WiFi.mode(WIFI_AP_STA);
-
-    // Define the subnet mask for the WiFi network
     const IPAddress subnet_mask(255, 255, 255, 0);
-
-    // Configure the soft access point with a specific IP and subnet mask
     WiFi.softAPConfig(local_ip, gateway_ip, subnet_mask);
-
-    // Start the soft access point with the given ssid, password, channel, max
-    // number of clients
     WiFi.softAP(ssid, password, WIFI_CHANNEL, 0, MAX_CLIENTS);
 
     delay(100);
