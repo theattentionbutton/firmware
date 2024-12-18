f219eb39738eaa9fb7eeaf944d5233859b618639
Siddharth Singh <me@shantaram.xyz>
2024-12-18 18:39:02 +0530

    InputEvents handles pinMode; no need to do it ourselves

--- src/constants.h
+++ src/constants.h
@@ -23,9 +23,12 @@ const IPAddress local_ip(4, 3, 2, 1);   // the IP address for the web server
 const IPAddress gateway_ip(4, 3, 2, 1); // IP address of the network
 const IPAddress subnet_mask(255, 255, 255, 0);
 
-const uint8_t pin_modes[][2] = {{ENC_SW, INPUT_PULLUP}, {ENC_DAT, INPUT},
-                                {ENC_CLK, INPUT},       {BZ1, OUTPUT},
-                                {MATRIX_DAT, OUTPUT},   {MATRIX_CLK, OUTPUT},
-                                {MATRIX_SEL, OUTPUT},   {MATRIX_CNT, OUTPUT}};
+const uint8_t pin_modes[][2] = {/* {ENC_SW, INPUT_PULLUP}, {ENC_DAT, INPUT},
+                                {ENC_CLK, INPUT},        */
+                                {BZ1, OUTPUT},
+                                {MATRIX_DAT, OUTPUT},
+                                {MATRIX_CLK, OUTPUT},
+                                {MATRIX_SEL, OUTPUT},
+                                {MATRIX_CNT, OUTPUT}};
 
 #endif
